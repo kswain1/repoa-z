2283ccfee25477e831f517e532b7afb13634c4f8
Kehlin <kehlinswain@gmail.com>
2019-02-18 21:26:50 -0600

    update to database to remove ephimeral behavior settings db url

--- athletes/settings.py
+++ athletes/settings.py
@@ -16,8 +16,8 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATABASE_URL = 'postgres://hjandlpzmpienu:0c3e550f0a4bcf6d1749a7709d8e5da778362fc4727cd07c5710f1d3cdf289ae@ec2' \
-               '-54-227-244-12.compute-1.amazonaws.com:5432/d80gibomf6hbh2'
+# DATABASE_URL = 'postgres://hjandlpzmpienu:0c3e550f0a4bcf6d1749a7709d8e5da778362fc4727cd07c5710f1d3cdf289ae@ec2' \
+#                '-54-227-244-12.compute-1.amazonaws.com:5432/d80gibomf6hbh2'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
